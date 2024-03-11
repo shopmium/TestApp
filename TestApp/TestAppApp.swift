8b5ddec7dc1934118e61894cac53ac3080803f1e
Sacha Arroche <sacha.arroche@shopmium.com>
2024-03-11 12:18:05 +0100

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Sacha Arroche on 11/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
